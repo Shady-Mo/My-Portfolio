2550b7678b82eb8342a282c2b5fa49e8decace87
Shady-Mo <shady.mohamed7899@gmail.com>
2024-03-24 23:52:45 +0200

    Create my portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ['"sora"', 'sans-serif'],
+        russo: ['"russo one"', 'sans-serif'],
+        openSans: ['"open sans"', 'sans-serif'],
+        poppins: ['"poppins"', 'sans-serif'],
+      },
+      fontSize: {
+        '9xl': '8rem', /* 128px */
+      }
+    },
+  },
+  plugins: [],
+}
+
