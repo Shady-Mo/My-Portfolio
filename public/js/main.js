9381e80562cfcd5b43c02f786f2a4edc1295e0a2
Shady-Mo <shady.mohamed7899@gmail.com>
2024-03-25 00:11:46 +0200

    Improving portfolio

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,46 @@
-function _0x26e4(_0x14249b,_0x52eac8){var _0xbe74d=_0xbe74();return _0x26e4=function(_0x26e4c2,_0x447faf){_0x26e4c2=_0x26e4c2-0x16b;var _0x29d644=_0xbe74d[_0x26e4c2];return _0x29d644;},_0x26e4(_0x14249b,_0x52eac8);}function _0xbe74(){var _0x104f12=['1404592bdWGQg','header\x20.container\x20.info\x20.image\x20.border','0deg','from','bounce.out','.container\x20.headline','-200%','length','3928116IUptCF','483025YyVKlY','.container\x20.headline\x20.links\x20li','3558425ldfuul','57303nEOoLv','2VzCcQi','1179AjEtkg','36NwRRSP','linear','styleSheets','animate','selectorText','timeline','562947vPJZea','style','cssRules','70bQOFpL','115880hjKTOc'];_0xbe74=function(){return _0x104f12;};return _0xbe74();}(function(_0x4b94b6,_0x49acdc){var _0x29b9e1=_0x26e4,_0x30d294=_0x4b94b6();while(!![]){try{var _0x3f7ced=-parseInt(_0x29b9e1(0x176))/0x1*(parseInt(_0x29b9e1(0x17a))/0x2)+-parseInt(_0x29b9e1(0x179))/0x3*(-parseInt(_0x29b9e1(0x17c))/0x4)+-parseInt(_0x29b9e1(0x178))/0x5+parseInt(_0x29b9e1(0x175))/0x6+-parseInt(_0x29b9e1(0x16d))/0x7+parseInt(_0x29b9e1(0x16c))/0x8*(parseInt(_0x29b9e1(0x17b))/0x9)+-parseInt(_0x29b9e1(0x16b))/0xa*(parseInt(_0x29b9e1(0x182))/0xb);if(_0x3f7ced===_0x49acdc)break;else _0x30d294['push'](_0x30d294['shift']());}catch(_0x58aa34){_0x30d294['push'](_0x30d294['shift']());}}}(_0xbe74,0xecf1d),$(document)['ready'](function(){function _0x26783b(_0x5d4aed){var _0x48b3d2=_0x26e4;function _0x4dca51(_0x528080,_0x147ea7){var _0x31eb93=_0x26e4;if(_0x528080['cssRules'])for(var _0x3cd7d0=0x0;_0x3cd7d0<_0x528080[_0x31eb93(0x184)][_0x31eb93(0x174)];_0x3cd7d0++){if(_0x528080[_0x31eb93(0x184)][_0x3cd7d0][_0x31eb93(0x180)]==_0x147ea7)return _0x528080[_0x31eb93(0x184)][_0x3cd7d0][_0x31eb93(0x183)];if(_0x528080[_0x31eb93(0x184)][_0x3cd7d0][_0x31eb93(0x184)]){var _0x23c2c5=_0x4dca51(_0x528080[_0x31eb93(0x184)][_0x3cd7d0],_0x147ea7);if(_0x23c2c5)return _0x23c2c5;}}return![];}for(var _0x44d9d8=0x0;_0x44d9d8<document[_0x48b3d2(0x17e)]['length'];_0x44d9d8++){var _0x4d4578=document[_0x48b3d2(0x17e)][_0x44d9d8];if(_0x4d4578[_0x48b3d2(0x184)]){var _0x4aad57=_0x4dca51(_0x4d4578,_0x5d4aed);if(_0x4aad57)return _0x4aad57;}}return![];}function _0x24b704(){var _0x23cae6=_0x26e4;let _0x76da31=gsap[_0x23cae6(0x181)]();_0x76da31['from'](_0x23cae6(0x172),{'duration':0x2,'y':_0x23cae6(0x173),'ease':_0x23cae6(0x171)}),_0x76da31[_0x23cae6(0x170)](_0x23cae6(0x177),{'duration':0x2,'opacity':0x0,'stagger':0.5});}_0x24b704(),$('header\x20.container\x20.info\x20.image')['on']({'mouseenter':function(){var _0x6b11bc=_0x26e4;$(_0x6b11bc(0x16e))[_0x6b11bc(0x17f)]({'rotate':'360deg','ease':'linear'},0x1f4);},'mouseleave':function(){var _0x8aa1e3=_0x26e4;$(_0x8aa1e3(0x16e))[_0x8aa1e3(0x17f)]({'rotate':_0x8aa1e3(0x16f),'ease':_0x8aa1e3(0x17d)},0x1f4);}});}));
+$(document).ready(function() {
+    function findCssRule(selectorString) {
+        // helper function searches through the document stylesheets looking for selectorString
+        // will also recurse through sub-rules (such as rules inside media queries)
+        function recurse(node, selectorString) {
+            if (node.cssRules) {
+                for (var i = 0; i < node.cssRules.length; i++) {
+                    if (node.cssRules[i].selectorText == selectorString) {
+                        return node.cssRules[i].style;
+                    }
+                    if (node.cssRules[i].cssRules) {
+                        var found = recurse(node.cssRules[i], selectorString);
+                        if (found) return found;
+                    }
+                }
+            }
+            return false;
+        }
+        
+        for (var i = 0; i < document.styleSheets.length; i++) {
+            var sheet = document.styleSheets[i];
+            if (sheet.cssRules) {
+                var found = recurse(sheet,selectorString);
+                if (found) return found;
+            }
+        }
+        
+        return false;
+    }
+
+    function startAnimation() {
+        let timeline = gsap.timeline();
+        timeline.from('.container .headline', {duration: 2, y: '-200%', ease: 'bounce.out'});
+        timeline.from('.container .headline .links li', {duration: 2, opacity: 0, stagger: 0.5})
+    }
+    startAnimation();
+
+    $("header .container .info .image").on({
+        mouseenter: function() {
+            $("header .container .info .image .border").animate({rotate: "360deg", ease: "linear"}, 500)
+        },
+        mouseleave: function() {
+            $("header .container .info .image .border").animate({rotate: "0deg", ease: "linear"}, 500)
+        },
+    });
+});
